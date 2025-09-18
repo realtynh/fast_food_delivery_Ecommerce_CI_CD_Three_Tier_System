535792d1a058d68257627a239a0ac8fbe6829d7b
Thanh Tinh <ntt23042004@gmail.com>
2025-09-18 22:30:01 +0700

    first commit

---
+++ Fast_Food_Delivery/backend/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+
+
+
+
+
+// app config
+const app = express()
+const port = 4000
+
+//moddleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food",foodRouter)
+
+app.get("/",(req,res)=> {
+    res.send("API Working")
+})
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
+
+//mongodb+srv://ThanhTinh:57819234@cluster0.qp2vzrq.mongodb.net/?
---
+++ Fast_Food_Delivery/frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'
+import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+const App = () => {
+  const [showLogin,setShowLogin] = React.useState(false);
+  return (
+    <>  
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className='app'>
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
