4da6f1acddb9497734c9289ca042b20309fdcd7e
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-07 20:24:41 +0700

    Update main.jsx

--- Fast_Food_Delivery/frontend/src/main.jsx
+++ Fast_Food_Delivery/frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter} from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
+import StoreContextProvider from './Context/StoreContext.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
 
