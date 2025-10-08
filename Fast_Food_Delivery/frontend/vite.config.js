5aaf36d8850e73d08b6703a76d4926de5650f1f8
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-08 23:31:43 +0700

    Update vite.config.js

--- Fast_Food_Delivery/frontend/vite.config.js
+++ Fast_Food_Delivery/frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fast_food_delivery_Ecommerce_CI_CD_Three_Tier_System/',
 })
