856827260598c5d4e7e3047f5c48198b95c50c64
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-08 23:56:43 +0700

    Update server.js

--- Fast_Food_Delivery/backend/server.js
+++ Fast_Food_Delivery/backend/server.js
@@ -16,7 +16,26 @@ const port = 4000
 //middleware
 app.use(express.json())
 app.use(cors())
+//------------------------------------
+//  Cấu hình CORS cho frontend github pages
+const allowedOrigins = [
+  "https://realtynh.github.io", // frontend trên GitHub Pages
+  "https://fast-food-delivery-ecommerce-ci-cd.vercel.app", // backend domain chính trên Vercel (optional)
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    // Cho phép nếu không có origin (Postman) hoặc nằm trong danh sách
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+// -----------------------------------
 // db connection
 connectDB();
 
