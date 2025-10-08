ce17f34788325ff52a48df1918ca1fa70251d60b
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-09 01:38:54 +0700

    Update db.js

--- Fast_Food_Delivery/backend/config/db.js
+++ Fast_Food_Delivery/backend/config/db.js
@@ -5,5 +5,5 @@ export const connectDB = async () => {
     // the string which insert in connect function . we can get it in mongodb atlas cloud in 'network access' field.
     // delete '?' and insert name of project behind the '/'
     //await mongoose.connect('mongodb+srv://ThanhTinh:57819234@cluster0.qp2vzrq.mongodb.net/fast-food-delivery').then(()=>{console.log("DB connect")})
-    await mongoose.connect('mongodb+srv://ThanhTinh:57819234@cluster0.qp2vzrq.mongodb.net/fast-food-delivery').then(()=>{console.log("DB connect")})
+    await mongoose.connect(process.env.MONGODB_URL).then(()=>{console.log("DB connect")})
 }
