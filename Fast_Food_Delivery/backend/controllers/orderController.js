82a5d26b7bf26442b9760253dece7ee2b90f93c5
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-15 18:09:53 +0700

    Update orderController.js

--- Fast_Food_Delivery/backend/controllers/orderController.js
+++ Fast_Food_Delivery/backend/controllers/orderController.js
@@ -6,7 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing  user order for frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5174"
+    //const frontend_url = "http://localhost:5174"
+    const frontend_url ="https://realtynh.github.io/fast_food_delivery_Ecommerce_CI_CD_Three_Tier_System"
 
     try {
         const newOrder = new orderModel({
