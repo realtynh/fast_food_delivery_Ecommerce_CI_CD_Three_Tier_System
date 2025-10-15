4582b1449c79cb58600974d507d0c0535dd5a4dc
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-15 16:26:25 +0000

    tracking logs and activate debug logs

--- Fast_Food_Delivery/backend/config/db.js
+++ Fast_Food_Delivery/backend/config/db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 
+
 export const connectDB = async () => {
+    // theo dõi logs. bật debug in tất cả ra console.
+    mongoose.set('debug', true);
     // the string which insert in connect function . we can get it in mongodb atlas cloud in 'network access' field.
     // delete '?' and insert name of project behind the '/'
     await mongoose.connect('mongodb+srv://ThanhTinh:57819234@cluster0.qp2vzrq.mongodb.net/fast-food-delivery').then(()=>{console.log("DB connect")})
-    //await mongoose.connect(process.env.MONGODB_URL).then(()=>{console.log("DB connect")})
+    
+
 }
