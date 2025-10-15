55ab102ec14c42ec31f0d94ee4e6fa725e25ba26
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-15 23:02:19 +0700

    Update server.js

--- Fast_Food_Delivery/backend/server.js
+++ Fast_Food_Delivery/backend/server.js
@@ -51,7 +51,7 @@ app.use('/images', express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
-app.use("health",healthRoute)
+app.use("/health",healthRoute)
 
 
 app.get("/",(req,res)=> {
