2d0ea6686388182cea618dc596ddd9ada1aa9576
realtynh <148040267+realtynh@users.noreply.github.com>
2025-10-15 17:56:27 +0700

    Update server.js

--- Fast_Food_Delivery/backend/server.js
+++ Fast_Food_Delivery/backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 // app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //------------------------------------
 //  CORS cấu hình cụ thể cho GitHub Pages
