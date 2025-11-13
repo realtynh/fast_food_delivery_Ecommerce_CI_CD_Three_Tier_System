4116f0017edf9ce8b822a5de82ccb677264f8e26
realtynh <ntt23042004@gmail.com>
2025-11-13 18:54:24 +0700

    add sentry to monitor frontend

--- Fast_Food_Delivery/frontend/src/main.jsx
+++ Fast_Food_Delivery/frontend/src/main.jsx
@@ -2,14 +2,46 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
+import * as Sentry from "@sentry/react";
+
+// https://fast-food-delivery-ecommerce-ci-cd-three-om5v.onrender.com
+
+Sentry.init({
+
+  dsn: "https://e8dfa5bb37fe666be4eee85011a7cb15@o4510194556796928.ingest.us.sentry.io/4510194577637376",
+
+  // Setting this option to true will send default PII data to Sentry.
+
+  // For example, automatic IP address collection on events
+
+  sendDefaultPii: true,
+
+  integrations: [
+
+    Sentry.browserTracingIntegration()
+
+  ],
+
+// Tracing
+
+tracesSampleRate: 1.0, // Capture 100% of the transactions
+
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+
+  tracePropagationTargets: ["localhost", /^https:\/\/fast-food-delivery-ecommerce-ci-cd-three-om5v\.onrender\.com\/api/],
+
+  // Enable logs to be sent to Sentry
+
+  enableLogs: true
+});
 ReactDom.createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
-  <StoreContextProvider>
-    <App />
-  </StoreContextProvider>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 )
