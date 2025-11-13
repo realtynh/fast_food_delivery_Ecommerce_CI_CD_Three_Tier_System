dbfb828c43c3c1a377d031aaba2bbe5cc40fc9a2
realtynh <ntt23042004@gmail.com>
2025-11-13 19:01:55 +0700

    create error to check

--- Fast_Food_Delivery/frontend/src/App.jsx
+++ Fast_Food_Delivery/frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
+import ButtonErrorCheck from './pages/ButtonErrorCheck/ButtonErrorCheck'
 const App = () => {
   const [showLogin,setShowLogin] = React.useState(false);
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Route path='/order' element={<PlaceOrder/>}/>
         <Route path='/verify' element={<Verify/>}/>
         <Route path='/myorders' element={<MyOrders/>}/>
+        <Route path='/buttonErrorChecks' element={<ButtonErrorCheck/>}/>
       </Routes>
     </div>
       <Footer/>
