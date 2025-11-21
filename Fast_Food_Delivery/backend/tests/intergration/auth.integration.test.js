919081a8c32504a9834878f9e4e4d0b2873e6782
realtynh <ntt23042004@gmail.com>
2025-11-21 08:39:55 +0700

    loi khoi tao tesst

--- Fast_Food_Delivery/backend/tests/intergration/auth.integration.test.js
+++ Fast_Food_Delivery/backend/tests/intergration/auth.integration.test.js
@@ -3,7 +3,7 @@ import app from '../../server.js';
 import userModel from '../../models/userModel.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-// import * as Sentry from "@sentry/node";
+import * as Sentry from "@sentry/node";
 
 
 jest.setTimeout(30000);
@@ -16,9 +16,9 @@ describe('User API Integration', () => {
     afterEach(() => jest.clearAllMocks());
 
     //  Chờ Sentry gửi dữ liệu trước khi tắt Jest
-    //   afterAll(async () => {
-    //     await Sentry.close(20000); // Chờ tối đa 2 giây
-    //   });
+      afterAll(async () => {
+        await Sentry.close(20000); // Chờ tối đa 2 giây
+      });
     // --------------------------------------------------------
 
     it('POST /api/user/register should return success and token', async () => {
@@ -61,26 +61,26 @@ describe('User API Integration', () => {
         expect(res.body).toEqual({ success: false, message: 'Invalid credentials' });
     });
 // test sentry
-// it('CI/CD Sentry Check: Should fail intentionally and report to Dashboard', async () => {
-//         userModel.findOne.mockResolvedValue({ _id: 'userId', password: 'hashedPassword' });
-//         bcrypt.compare.mockResolvedValue(true);
-//         jwt.sign.mockReturnValue('fakeToken');
+it('CI/CD Sentry Check: Should fail intentionally and report to Dashboard', async () => {
+        userModel.findOne.mockResolvedValue({ _id: 'userId', password: 'hashedPassword' });
+        bcrypt.compare.mockResolvedValue(true);
+        jwt.sign.mockReturnValue('fakeToken');
 
-//         const res = await request(app)
-//             .post('/api/user/login')
-//             .send({ email: 'test@example.com', password: '12345678' });
+        const res = await request(app)
+            .post('/api/user/login')
+            .send({ email: 'test@example.com', password: '12345678' });
 
-//         try {
-//             // Mong đợi 500 để gây lỗi
-//             expect(res.statusCode).toBe(500); 
-//         } catch (error) {
-//             console.error("Test Failed. Sending to Sentry...");
+        try {
+            // Mong đợi 500 để gây lỗi
+            expect(res.statusCode).toBe(500); 
+        } catch (error) {
+            console.error("Test Failed. Sending to Sentry...");
             
-//             // Gửi lỗi
-//             Sentry.captureException(new Error(`CI/CD Test Failed Assertion: ${error.message}`));
+            // Gửi lỗi
+            Sentry.captureException(new Error(`CI/CD Test Failed Assertion: ${error.message}`));
             
-//             throw error;
-//         }
-//     });
+            throw error;
+        }
+    });
 
 });
